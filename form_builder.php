5a89180162a0a43126375460604d3a4c6464d730
Mohan Raj <mohanraj@elula.tech>
2022-07-02 15:23:32 +0530

    Create plugin main file and add headers

---
+++ form_builder.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:       Form Builder
+ * Plugin URI:        https://github.com/praem90/form-builder-wordpress-plugin
+ * Description:       Allows you create dynamic forms
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Mohan Raj
+ * Author URI:        https://github.com/praem90
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       form-builder
+ * Domain Path:       /languages
+ */
